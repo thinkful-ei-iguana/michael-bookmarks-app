abe8d798670e4bd5c7b22ecf49c6d64af65f367b
Michael Weedman <michael.weedman@gmail.com>
2019-11-07 11:53:20 -0700

    updated API
    added error container

--- src/api.js
+++ src/api.js
@@ -1,38 +1,54 @@
 import cuid from 'cuid';
 import $ from 'jquery';
+import handleFeatures from './handleFeatures';
+
 
 const baseURL = 'https://thinkful-list-api.herokuapp.com/michael/bookmarks';
 
-//default fetch expression
-function apiFetch(...args){
+//default fetch expression ***WORKS
+// function apiFetch(...args){
+//   let error;
+//   return fetch(...args)
+//     .then(res =>{
+//       if (!res.ok){
+//         error = { code: res.status };
+//       }
+//       return res.json();
+//     })
+//     .then(data => {
+//       if (error) {
+//         error.message = data.message;
+//         return Promise.rejects(error);
+//       }
+//       return data;
+//     });
+// }
+
+const apiFetch = function (...args) {
   let error;
   return fetch(...args)
-    .then(res =>{
-      if (!res.ok){
+    .then(res => {
+      if (!res.ok) {
         error = { code: res.status };
+        if (!res.headers.get('content-type').includes('json')) {
+          error.message = res.statusText;
+          return Promise.reject(error);
+        }
       }
       return res.json();
     })
     .then(data => {
       if (error) {
         error.message = data.message;
-        return Promise.rejects(error);
+        return Promise.reject(error);
       }
       return data;
     });
-}
+};
 
 const getBookmarks = function(){
   return apiFetch(`${baseURL}`);
 };
-
-//newData needs to be formatted as an object that we can post in
-//the following format:
-//{ "title": inputTitle,
-//  "desc": inputDesc,
-//  "rating": inputRating,
-//  "url": inputURL
-//}
   
 const createBookmark = function(newData){
   let newBookmark = JSON.stringify(newData);
